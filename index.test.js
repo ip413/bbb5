f4305f86d77393545807601d9c43d6eeb56cac4f
Andrzej Salak <andrzej.salak@gmail.com>
2021-05-29 00:50:16 +0200

    test: add sample test

---
+++ index.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+const expect = require('chai').expect;
+const calc = require('./index');
+
+describe('bbb5', function () {
+  it('todo', function () {
+    expect(calc.calcDistance()).to.equal('TODO');
+  });
+});
