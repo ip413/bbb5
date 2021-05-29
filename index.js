20792ae98aa928007bc93ffd6aaa5789dd5ee36d
Andrzej Salak <andrzej.salak@gmail.com>
2021-05-29 10:40:48 +0200

    feat: calc distance between two pixels

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-exports.calcDistance = () => 'TODO';
+/**
+ * n x m
+ * i lines, j columns
+ * @returns
+ */
+
+exports.calcDistanceToNearest = () => 'TODO';
+
+/**
+ * |i1-i2|+|j1-j2|p
+ * @param {*} pixel1
+ * @param {*} pixel2
+ */
+exports.calcDistanceBetween = (pixel1, pixel2) => {
+    return Math.abs(pixel1.i - pixel2.i) + Math.abs(pixel1.j - pixel2.j);
+}
+
+exports.getPixel = (i, j) => {
+    return {i, j}
+}
--- index.test.js
+++ index.test.js
@@ -3,7 +3,49 @@ const expect = require('chai').expect;
 const calc = require('./index');
 
 describe('bbb5', function () {
-  it('todo', function () {
-    expect(calc.calcDistance()).to.equal('TODO');
+  it.skip('calcDistanceToNearest', function () {
+    expect(calc.calcDistanceToNearest()).to.equal('TODO');
+  });
+
+  it('calcDistanceBetween', function () {
+    const pixelsSet1 = getSetOfPixels([1, 1, 2, 2]);
+    expect(calc.calcDistanceBetween(...pixelsSet1)).to.equal(2);
+
+    const pixelsSet2 = getSetOfPixels([1, 1, 3, 3]);
+    expect(calc.calcDistanceBetween(...pixelsSet2)).to.equal(4);
+
+    const pixelsSet3 = getSetOfPixels([1, 1, 4, 4]);
+    expect(calc.calcDistanceBetween(...pixelsSet3)).to.equal(6);
   });
 });
+
+
+
+function getSetOfPixels (positions) {
+  const pixels =[];
+  for(let i = 0; i < positions.length; i+=2) {
+    pixels.push(calc.getPixel(positions[i], positions[i + 1]))
+  }
+  return pixels;
+}
+/*
+sample, distance 2
+1, 0
+0, 1
+*/
+
+
+/*
+sample, distance 4
+1, 0, 0
+0, 0, 0
+0, 0, 1
+*/
+
+/*
+sample, distance 6
+1, 0, 0, 0
+0, 0, 0, 0
+0, 0, 0, 0
+0, 0, 0, 1
+*/
